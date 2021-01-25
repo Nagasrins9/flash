55ef2f3025279375f368c9edb1c364cd746649eb
Nagasri N <nagasrins9@gmail.com>
2021-01-25 18:01:37 +0530

    First Commit
    Uploading all files

---
+++ hello_nan/src/test/java/hello_nan/TestHellonan.java
@@ -0,0 +1,12 @@
+package hello_nan;
+
+import org.testng.annotations.Test;
+
+public class TestHellonan {
+	
+	@Test
+	public void testHellonan() {
+		System.out.println("Nandhu is beautiful!!");
+	}
+
+}
